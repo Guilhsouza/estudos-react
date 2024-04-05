8ff79750a9b2c0e761202ef3883c5ef4db5a6099
Guilherme Souza <gui.edu0380@gmail.com>
2024-04-04 22:55:58 -0300

    adicionando estudos trabalhando com vite

---
+++ utilizando-vite/testando-vite/src/App.tsx
@@ -0,0 +1,30 @@
+import reactLogo from "../public/react.png"
+
+function App() {
+  return (
+    <>
+      <img src={reactLogo} alt="react logo" width={180} />
+      <h1>React</h1>
+      <p>A biblioteca para interfaces de usuário web e nativas</p>
+      <button>Aprenda React</button>
+      <button>Referencia de API</button>
+      <hr />
+      <h3>Crie interfaces</h3>
+      <p>Lorem ipsum dolor sit amet consectetur adipisicing elit. Accusantium, provident.</p>
+      <hr />
+      <h3>Reutilize</h3>
+      <p>Lorem ipsum dolor sit amet consectetur adipisicing elit. Odit laborum rem fugit.</p>
+      <hr />
+      <h3>Proximos passos</h3>
+      <ul>
+        <li>Lorem ipsum dolor sit.</li>
+        <li>Lorem ipsum dolor sit.</li>
+        <li>Lorem ipsum dolor sit.</li>
+        <li>Lorem ipsum dolor sit.</li>
+        <li>Lorem ipsum dolor sit.</li>
+      </ul>
+    </>
+  )
+}
+
+export default App
