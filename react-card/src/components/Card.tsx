f013d7bac7e9d9a21d5f0fa7ffd1da0a35ba106f
Guilherme Souza <gui.edu0380@gmail.com>
2024-04-11 21:28:00 -0300

    adicionando atividade em que criei um card com react

---
+++ react-card/src/components/Card.tsx
@@ -0,0 +1,20 @@
+import styles from '../../styles/card.module.css'
+
+export default function Card() {
+
+    return (
+        <div className={styles.card}>
+            <img className={styles.img} src="https://zootecniaprecisao.com.br/wp-content/uploads/2023/06/Fotos-abelha-polen-flor-cores-asa.webp" alt="Fotinha das abelhas" />
+            <div>
+                <h1 className={styles.tittle}>Importância das Abelhas</h1>
+                <p className={styles.description}>As abelhas desempenham um papel crucial na polinização de plantas, contribuindo para a reprodução de uma grande
+                    variedade de culturas alimentares. Sem elas, muitas plantas não poderiam produzir frutos e sementes, impactando
+                    diretamente a produção de alimentos. Além disso, as abelhas são essenciais para a manutenção da biodiversidade e
+                    funcionamento dos ecossistemas. Seu trabalho incansável como polinizadoras também apoia a produção de mel, cera e
+                    outros produtos apícolas. Preservar as populações de abelhas é vital para a segurança alimentar global e a saúde dos
+                    ecossistemas.</p>
+                <a href="https://www.acaatinga.org.br/voce-sabe-qual-a-importancia-das-abelhas/" target='_blank'><button className={styles.btn}>Saiba mais!</button></a>
+            </div>
+        </div>
+    )
+}
