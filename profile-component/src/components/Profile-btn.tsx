b594ad885226b6871006c79e7ddb1051a8077af9
Guilherme Souza <gui.edu0380@gmail.com>
2024-04-20 17:55:56 -0300

    adicionando estilos do botao direto na pasta Profile.tsx

--- profile-component/src/components/Profile-btn.tsx
+++
@@ -1,7 +0,0 @@
-import styles from '../styles/profile-btn.module.css'
-
-export default function ProfileBtn(btnContent: string) {
-    return (
-        <button className={styles.profileBtn}>{btnContent}</button>
-    )
-}
