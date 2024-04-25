16c634898ef6a098a818c8d26d32ea47c4a8d5f8
Guilherme Souza <gui.edu0380@gmail.com>
2024-04-25 16:20:30 -0300

    adicionado props children

--- profile-component/src/components/Profile.tsx
+++ profile-component/src/components/Profile.tsx
@@ -1,5 +1,6 @@
 import btnStyles from "../styles/profile-btn.module.css"
 import profileStyles from "../styles/profile-styles.module.css"
+import ProfileSection from "./ProfileSection"
 
 export default function Profile(props: {
     avatar: string, name: string, bio: string, email: string, phone: string,
@@ -10,14 +11,14 @@ export default function Profile(props: {
             <div className={profileStyles.profileComponent}>
                 <img src={props.avatar} alt="imagem de perfil" />
                 <h3>{props.name}</h3>
-                <div>{props.bio}</div>
-                <div>{props.phone}</div>
-                <div>{props.email}</div>
-                <div>
+                <ProfileSection>{props.bio}</ProfileSection>
+                <ProfileSection>{props.phone}</ProfileSection>
+                <ProfileSection>{props.email}</ProfileSection>
+                <ProfileSection>
                     <a href={props.githubUrl} target="_blank"><button className={btnStyles.profileBtn}>GitHub</button></a><br />
                     <a href={props.linkedinUrl} target="_blank"><button className={btnStyles.profileBtn}>LinkedIn</button></a><br />
                     <a href={props.facebookUrl} target="_blank"><button className={btnStyles.profileBtn}>Facebook</button></a>
-                </div>
+                </ProfileSection>
             </div>
         </>
     )
---
+++ profile-component/src/components/ProfileSection/index.tsx
@@ -0,0 +1,9 @@
+import styles from './style.module.css'
+
+export default function ProfileSection(props: { children: React.ReactNode }) {
+    return (
+        <div className={styles.wrapper}>
+            {props.children}
+        </div>
+    )
+}
