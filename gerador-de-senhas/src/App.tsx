b58e1ca2380b50af8ea0e8c5c94e86ded682355d
Guilherme Souza <gui.edu0380@gmail.com>
2024-05-02 11:22:04 -0300

    usando o state para setar quantidade de caracteres na geração das senhas

--- gerador-de-senhas/src/App.tsx
+++ gerador-de-senhas/src/App.tsx
@@ -1,15 +1,15 @@
-import { useState } from 'react'
+import React, { useState } from 'react'
 import styles from './styles/generate.module.css'
 
 export default function App() {
   const [password, setPassword] = useState('')
   const [copyText, setCopyText] = useState('Copiar')
+  const [passwordSize, setPasswordSize] = useState(12)
 
   function generate() {
     const characters = "'1234567890-=!@#$%¨&*()_+qwertyuiop[asdfghjklç~]zxcvbnm,.;/QWERTYUIOP{ASDFGHJKLÇ^}ZXCVBNM<>:?"
-    const length = 12
     let newPassword = ""
-    for (let i = 0; i < length; i++) {
+    for (let i = 0; i < passwordSize; i++) {
       const position = Math.floor(Math.random() * characters.length)
       newPassword += characters[position]
       console.log(position)
@@ -27,6 +27,16 @@ export default function App() {
     <div className='app'>
       <div className={styles.container}>
         <h1 className={styles.title}>Gerador de Senhas</h1>
+        <div>
+          <label htmlFor='passwordSize'>Tamanho da senha: </label>
+          <input
+            type='number'
+            id='passwordSize'
+            min={1}
+            value={passwordSize}
+            onChange={(ev: React.ChangeEvent<HTMLInputElement>) => setPasswordSize(parseInt(ev.target.value))}
+          />
+        </div>
         <button className={`${styles.button} ${styles.buttonSpace}`} onClick={generate}>Gerar</button>
         <button className={styles.button} onClick={copyPassword}>{copyText}</button>
         <div className={styles.pswrd}>{password}</div>
