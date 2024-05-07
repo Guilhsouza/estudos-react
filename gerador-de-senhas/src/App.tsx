d7625caa30dbc92f6536e427ed588cc0dc6d4069
Guilherme Souza <gui.edu0380@gmail.com>
2024-05-07 11:44:09 -0300

    adicionando estudos de renderização condicional

--- gerador-de-senhas/src/App.tsx
+++ gerador-de-senhas/src/App.tsx
@@ -5,7 +5,10 @@ import Input from './components/Input'
 export default function App() {
   const [password, setPassword] = useState('')
   const [copyText, setCopyText] = useState('Copiar')
-  const [passwordSize, setPasswordSize] = useState(12)
+  const [customSize, setCustomSize] = useState(12)
+  const [showInput, setShowInput] = useState(0)
+
+  const passwordSize: number = showInput ? customSize : 12
 
   function generate() {
     const characters = "'1234567890-=!@#$%¨&*()_+qwertyuiop[asdfghjklç~]zxcvbnm,.;/QWERTYUIOP{ASDFGHJKLÇ^}ZXCVBNM<>:?"
@@ -13,7 +16,6 @@ export default function App() {
     for (let i = 0; i < passwordSize; i++) {
       const position = Math.floor(Math.random() * characters.length)
       newPassword += characters[position]
-      console.log(position)
     }
     setPassword(newPassword)
     setCopyText('Copiar')
@@ -29,10 +31,30 @@ export default function App() {
       <div className={styles.container}>
         <h1 className={styles.title}>Gerador de Senhas</h1>
         <div>
-          <label htmlFor='passwordSize'>Tamanho da senha: </label>
-          <Input passwordSize={passwordSize} setPasswordSize={setPasswordSize} />
+          <label htmlFor="showInput">Customizar tamanho:</label>
+          <input
+            type="checkbox"
+            id='showInput'
+            value={showInput}
+            onChange={() => setShowInput((currentValue) => {
+              if (currentValue === 0) {
+                return 1
+              }
+
+              return 0
+            })}
+          />
         </div>
-        <button className={`${styles.button} ${styles.buttonSpace}`} onClick={generate}>Gerar</button>
+
+        {showInput ? (
+          <div>
+            <label htmlFor='passwordSize'>Tamanho da senha: </label>
+            <Input passwordSize={customSize} setPasswordSize={setCustomSize} />
+          </div>
+        ) : null
+        }
+
+        <button className={`${styles.button} ${styles.buttonSpace}`} onClick={generate}>Gerar Senha</button>
         <button className={styles.button} onClick={copyPassword}>{copyText}</button>
         <div className={styles.pswrd}>{password}</div>
       </div>
