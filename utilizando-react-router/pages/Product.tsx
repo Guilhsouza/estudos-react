956e5730865c104383b2e5d71987c2587235a6e9
Guilherme Souza <gui.edu0380@gmail.com>
2024-07-09 22:04:59 -0300

    adicionando interface de tipos para evitar que product seja unknown

--- utilizando-react-router/pages/Product.tsx
+++ utilizando-react-router/pages/Product.tsx
@@ -1,20 +1,13 @@
-import { Link, useParams } from 'react-router-dom'
-import products from '../src/database.json'
+interface loaderBody {
+    name: string,
+    description: string,
+    price: number
+}
+
+import { Link, useLoaderData } from 'react-router-dom'
 
 export default function Product() {
-    const { productId } = useParams()
-
-    const product = products.find((product: { id: number | string }) => (product.id === +productId!))
-
-    if (!product) {
-        return (
-            <>
-                <h2>404 Not Found</h2>
-                <h4>Infelizmente o produto não foi encontrado...</h4>
-                <Link to='/products'><button>Voltar</button></Link>
-            </>
-        )
-    }
+    const product = useLoaderData() as loaderBody;
 
     return (
         <section>
