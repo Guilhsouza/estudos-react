53d5541e367072838fb831828f273bf759a11f5d
Guilherme Souza <gui.edu0380@gmail.com>
2024-07-22 12:10:10 -0300

    adicioando configurações do next para trabalhar com imagens

--- black-nextjs/next.config.mjs
+++ black-nextjs/next.config.mjs
@@ -1,6 +1,17 @@
 /** @type {import('next').NextConfig} */
 const nextConfig = {
   reactStrictMode: true,
+  images: {
+    domains: ['store.storeimages.cdn-apple.com']
+    // remotePatterns: [
+    //   {
+    //     protocol: 'https',
+    //     hostname: 'store.storeimages.cdn-apple.com',
+    //     port: '',
+    //     pathname: '/**'
+    //   }
+    // ]
+  }
 };
 
 export default nextConfig;
